9093d132a7ab76ff538b60762142d007fb76706c
patricia herreros <patri.hd@gmail.com>
2023-05-05 21:45:18 +0200

    pantalla olvido su contraseña

---
+++ app/src/main/java/com/example/registro_jornada_android/Olvidopass.java
@@ -0,0 +1,16 @@
+package com.example.registro_jornada_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Olvidopass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvidopass);
+    }
+
+
+}
